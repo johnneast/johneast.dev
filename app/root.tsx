d9e6195f25b1164c4fce2048e6785bb97624a537
John East <johnneast@gmail.com>
2025-03-14 15:55:20 -0400

    Adds logging for location

--- app/root.tsx
+++ app/root.tsx
@@ -67,6 +67,8 @@ export default function App() {
   const currentRouteLabel =
     navItems.find((item) => item.to === location.pathname)?.label || "Select";
 
+
+  console.log(`location.pathname: ${location.pathname}`);
   return (
     <div className="min-h-screen flex flex-col bg-background text-foreground">
       {/* Desktop Layout */}
@@ -121,6 +123,7 @@ export default function App() {
         <nav className="p-4 sticky top-0 z-10 bg-background">
           <Select
             value={location.pathname}
+            defaultValue={location.pathname}
             onValueChange={(value) => navigate(value)}
           >
             <SelectTrigger className="w-full">
