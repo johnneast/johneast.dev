274426fee62982908bdf44bb7e6cedef4526498c
John East <johnneast@gmail.com>
2025-03-26 18:35:44 -0400

    Adds location information to the about me bio

--- app/components/my-bio.tsx
+++ app/components/my-bio.tsx
@@ -3,15 +3,17 @@ export default function MyBio() {
     <div className="flex flex-col gap-4">
       <p>
         I'm a full stack developer with a back end leaning and a passion for crafting solutions that make a difference.
-        Over the years, I've worked with Java, Node.js, Python, Ruby on Rails, React, SQL and NoSQL databases, plus cloud platforms like AWS,
-        building everything from startup MVPs to large scale systems for global companies. I love leading teams of engineers,
-        shaping technical roadmaps, and getting hands on with code, whether it's a quick new project or a solid cloud solution.
-        A drive to learn new things has kept me going through years of tech changes, and it's still what fires me up.
+        Over the years, I've worked with Java, Node.js, Python, Ruby on Rails, React, SQL and NoSQL databases, plus
+        cloud platforms like AWS, building everything from startup MVPs to large scale systems for global companies.
+        Based near Toronto in Canada, I love leading teams of engineers, shaping technical roadmaps, and getting hands
+        on with code, whether it's a quick new project or a solid cloud solution. A drive to learn new things has kept
+        me going through years of tech changes, and it's still what fires me up.
       </p>
       <p>
-        Outside of code, I'm often on the mountain snowboarding in winter, playing tennis in summer, or chasing the perfect
-        coffee brew any time of year. That mix of focus and curiosity shows up in my work, blending tech skills with a touch of creativity.
-        Want to chat? Try my personal chatbot—ask me about my experience, the best powder day, my go-to coffee, or just how bad my tennis serve is :)
+        Outside of code, I'm often on the mountain snowboarding in winter, playing tennis in summer, or chasing the
+        perfect coffee brew any time of year. That mix of focus and curiosity shows up in my work, blending tech skills
+        with a touch of creativity. Want to chat? Try my personal chatbot—ask me about my experience, the best powder
+        day, my go-to coffee, or just how bad my tennis serve is :)
       </p>
     </div>
   );
